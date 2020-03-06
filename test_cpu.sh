1afc1e8508710d71192bcf38cd6c0265eb0b1bce
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2020-03-06 21:08:42 +0300

    Update test_cpu.sh

--- test_cpu.sh
+++ test_cpu.sh
@@ -5,6 +5,6 @@ echo "Estimated time: 30-40 minutes"
 mkdir -p ./results/cpu
 
 #CPU multi thread
-time for i in 1 2 4 6 8 12 16 24 32 48 64 96 128 192 256 320 512 ; do echo "CPU threads: $i" ; sysbench cpu --threads=$i --cpu-max-prime=30000 --events=10000000 --max-time=100 run > ./results/cpu/cpu_$i.txt ; done
+time for i in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do echo "CPU threads: $i" ; sysbench cpu --threads=$i --cpu-max-prime=30000 --events=10000000 --max-time=100 run > ./results/cpu/cpu_$i.txt ; done
 
 
