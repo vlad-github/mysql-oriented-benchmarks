b5330296fe20a98caabbd78ad44a9a91d9720130
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2021-03-18 11:37:08 +0300

    Added cache invalidation, changed threads count

--- test_fileio.sh
+++ test_fileio.sh
@@ -15,44 +15,46 @@ cd ./io_testfiles
 
 ### force prepare for clean tests
 echo "starting tests " >> ../results/fileio/fileio_prepare.log
-time sysbench --test=fileio --file-total-size=20G prepare >> ../results/fileio/fileio_prepare.log 2>&1
+time sysbench --test=fileio --file-total-size=80G prepare >> ../results/fileio/fileio_prepare.log 2>&1
 
 echo "Running tests"
 
-for m in seqrd rndrd seqwr ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
+for m in seqrd rndrd seqwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ; do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
 done ; done
 
-### restoring test tiles after write test
-time sysbench --test=fileio --file-total-size=20G prepare >> ../results/fileio/fileio_prepare.log 2>&1
-
-for m in rndwr ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
+for m in rndwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ; do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
 done ; done
 
 # rndwr / fsync=1
-for m in rndwr ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ;       do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t --file-fsync-freq=1 run > ../results/fileio/sysbench_fileio_"$m"_"$t"_fsync.txt ; 
+for m in rndwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;       do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t --file-fsync-freq=1 run > ../results/fileio/sysbench_fileio_"$m"_"$t"_fsync.txt ; 
 done ; done
 
 # seqrewr / block = 512
-for m in seqrewr ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ;     do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-extra-flags=direct --file-block-size=512 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_512.txt ; 
+for m in seqrewr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;     do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-block-size=512 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_512.txt ; 
 done ; done
 
 # seqrewr / block = 4096
-for m in seqrewr ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ;     do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-extra-flags=direct --file-block-size=4096 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_4k.txt ; 
+for m in seqrewr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;     do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-block-size=4096 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_4k.txt ; 
 done ; done
 
 # seqrewr / block = 512, nodirect
-for m in seqrewr ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ;     do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-block-size=512 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_512_nodirect.txt ; 
+for m in seqrewr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;     do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-block-size=512 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_512_nodirect.txt ; 
 done ; done
 
 # direct / rw ratio = 4
-# old threads 1 2 4 6 8 12 16 24 32 48 64 98 128 190 256 384 512
-for m in rndrw ; do for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ;       do echo "Test: $m threads: $t" ; 
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=20G --file-extra-flags=direct --file-rw-ratio=4 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_direct_rw4.txt ; 
+for m in rndrw ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;       do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-rw-ratio=4 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_direct_rw4.txt ; 
 done ; done
-
