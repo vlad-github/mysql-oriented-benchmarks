d8408a972e54b26e22a09c9d215c83c3b41188eb
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2021-04-02 12:42:40 +0300

    minumal blocksize changed from 512 bytes to 4096
    + file size increased from 80Gb to 120Gb
    + added seqwr with fsync test

--- test_fileio.sh
+++ test_fileio.sh
@@ -15,46 +15,59 @@ cd ./io_testfiles
 
 ### force prepare for clean tests
 echo "starting tests " >> ../results/fileio/fileio_prepare.log
-time sysbench --test=fileio --file-total-size=80G prepare >> ../results/fileio/fileio_prepare.log 2>&1
+time sysbench --test=fileio --file-total-size=120G prepare >> ../results/fileio/fileio_prepare.log 2>&1
 
 echo "Running tests"
 
 for m in seqrd rndrd seqwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ; do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-block-size=16384 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
 done ; done
 
+# seqwr + fsync
+for m in seqwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ; do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-block-size=16384 --file-test-mode=$m --time=30 --threads=$t --file-fsync-freq=1 run > ../results/fileio/sysbench_fileio_"$m"_"$t"_fsync.txt ; 
+done ; done
+
+# rndwr / fsync=0
 for m in rndwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ; do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-block-size=16384 --file-test-mode=$m --time=30 --threads=$t --file-fsync-freq=0 run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
 done ; done
 
 # rndwr / fsync=1
 for m in rndwr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;       do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-test-mode=$m --time=30 --threads=$t --file-fsync-freq=1 run > ../results/fileio/sysbench_fileio_"$m"_"$t"_fsync.txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-block-size=16384 --file-test-mode=$m --time=30 --threads=$t --file-fsync-freq=1 run > ../results/fileio/sysbench_fileio_"$m"_"$t"_fsync.txt ; 
 done ; done
 
-# seqrewr / block = 512
+# seqrewr / block = 16384
 for m in seqrewr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;     do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-block-size=512 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_512.txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-block-size=16384 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t".txt ; 
 done ; done
 
 # seqrewr / block = 4096
 for m in seqrewr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;     do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-block-size=4096 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_4k.txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-block-size=4096 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_4k.txt ; 
 done ; done
 
-# seqrewr / block = 512, nodirect
+# seqrewr / block = 16384, nodirect
 for m in seqrewr ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;     do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-block-size=512 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_512_nodirect.txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-block-size=16384 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_16k_nodirect.txt ; 
 done ; done
 
 # direct / rw ratio = 4
 for m in rndrw ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;       do echo "Test: $m threads: $t" ; 
     echo 3 > /proc/sys/vm/drop_caches
-    sysbench --test=fileio --file-io-mode=sync --file-total-size=80G --file-extra-flags=direct --file-rw-ratio=4 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_direct_rw4.txt ; 
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-rw-ratio=4 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_direct_rw4.txt ; 
+done ; done
+
+# direct / rw ratio = 4
+for m in rndrw ; do for t in 1 2 4 6 8 10 12 16 20 24 32 48 64 96 128 160 192 256 320 384 448 512 ;       do echo "Test: $m threads: $t" ; 
+    echo 3 > /proc/sys/vm/drop_caches
+    sysbench --test=fileio --file-io-mode=sync --file-total-size=120G --file-extra-flags=direct --file-rw-ratio=4 --file-test-mode=$m --time=30 --threads=$t run > ../results/fileio/sysbench_fileio_"$m"_"$t"_direct_rw4.txt ; 
 done ; done
