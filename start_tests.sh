82de6c379d5f74654b2dabc352a739c2d6480071
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2021-04-04 17:24:41 +0300

    line up info file ext

--- start_tests.sh
+++ start_tests.sh
@@ -4,7 +4,7 @@
 mkdir -p ./results/general_info
 pt-summary > ./results/general_info/pt-summary.info
 # in case we're in AWS
-wget -q -O - http://169.254.169.254/latest/meta-data/instance-type > ./results/general_info/aws-instance-type
+wget -q -O - http://169.254.169.254/latest/meta-data/instance-type > ./results/general_info/aws-instance-type.info
 
 bash ./test_cpu.sh
 bash ./test_fileio.sh
