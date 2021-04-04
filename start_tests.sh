e2d15aee08166888bc054ca5108e98606deac058
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2021-04-04 17:24:04 +0300

    Collect some general info from the system (where available)

--- start_tests.sh
+++ start_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+### Getting some general info about the system
+mkdir -p ./results/general_info
+pt-summary > ./results/general_info/pt-summary.info
+# in case we're in AWS
+wget -q -O - http://169.254.169.254/latest/meta-data/instance-type > ./results/general_info/aws-instance-type
 
 bash ./test_cpu.sh
 bash ./test_fileio.sh
