39254ef452ebdf11939fc69cb3434708ff793e9c
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2021-04-07 16:10:52 +0300

    recreate database every test (~8 minutes)

--- test_in-ram_sql.sh
+++ test_in-ram_sql.sh
@@ -32,7 +32,7 @@ mkdir -p ./results/sql
 
 SBCOUNT=`mysql $MYSQL_DATABASE --user=$MYSQL_USER --password=$MYSQL_PASS -NB -e "select COUNT(*) from sbtest1"`
 
-#mysql -u $MYSQL_USER --password=$MYSQL_PASS -e "DROP DATABASE IF EXISTS sbtest;CREATE DATABASE sbtest;"
+mysql -u $MYSQL_USER --password=$MYSQL_PASS -e "DROP DATABASE IF EXISTS sbtest; CREATE DATABASE sbtest;"
 
 if [[ ! "$SBCOUNT" -gt 1 ]] ; then
         echo "Test data not detected, running prepare"
