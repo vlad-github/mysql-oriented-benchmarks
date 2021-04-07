6d4b1050c229a08798b2a569c9e237651e907a11
Vladimir Fedorkov <vlad-github@users.noreply.github.com>
2021-04-07 11:11:14 +0300

    Prepare condition fixed, added some formatting

--- test_sql.sh
+++ test_sql.sh
@@ -24,19 +24,38 @@ mv ./results/sql/oltp_*.txt ./results/sql/`date +%F`/
 
 mkdir -p ./results/sql
 
-SBCOUNT=`mysql sbtest --user=$MYSQL_USER --password=$MYSQL_PASS -NB -e "select COUNT(*) from sbtest"`
+SBCOUNT=`mysql $MYSQL_DATABASE --user=$MYSQL_USER --password=$MYSQL_PASS -NB -e "select COUNT(*) from sbtest1"`
 
 #mysql -u $MYSQL_USER --password=$MYSQL_PASS -e "DROP DATABASE IF EXISTS sbtest;CREATE DATABASE sbtest;"
 
 if [[ ! "$SBCOUNT" -gt 1 ]] ; then
         echo "Test data not detected, running prepare"
-        time sysbench --test=/usr/share/sysbench/oltp_point_select.lua prepare --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASS --tables=64 --table-size=10000000 --threads=16 > ./results/sql/oltp_prepare.log 2>&1
+        time sysbench --test=/usr/share/sysbench/oltp_point_select.lua prepare \
+                --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASS \
+                --tables=64 --table-size=10000000 --threads=16 > ./results/sql/oltp_prepare.log 2>&1
 else
         echo "$SBCOUNT: OK, prepare already completed"
 fi
 
 
-sysbench --test=/usr/share/sysbench/oltp_point_select.lua run --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASS --tables=64 --table-size=10000000 --threads=1 --max-time=100 > ./results/sql/oltp_point_select_cold_run_single_thread.txt 
+# warmup
+sysbench --test=/usr/share/sysbench/oltp_point_select.lua run \
+        --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASS \
+        --tables=64 --table-size=10000000 --threads=1 \
+        --max-time=100 > ./results/sql/oltp_point_select_cold_run_single_thread.txt 
 
-for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do echo -n "RO/treads=$t:\t" ; sysbench --test=/usr/share/sysbench/oltp_point_select.lua run --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASS --tables=64 --table-size=10000000 --threads=$t --max-time=100 > ./results/sql/oltp_point_select_"$t".txt ; sleep 10 ; done
-for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do echo -n "RW/threads=$t:\t" ; sysbench --test=/usr/share/sysbench/oltp_read_write.lua run --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASS --tables=64 --table-size=10000000 --threads=$t --max-time=100 > ./results/sql/oltp_read_write_"$t".txt ; sleep 10 ; done
+for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do 
+        echo -n "RO/treads=$t:\t" ; 
+        sysbench --test=/usr/share/sysbench/oltp_point_select.lua run --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER \
+                --mysql-password=$MYSQL_PASS --tables=64 --table-size=10000000 --threads=$t \
+                --max-time=100 > ./results/sql/oltp_point_select_"$t".txt ; 
+        sleep 10 ; 
+done
+
+for t in 1 2 4 6 8 12 16 24 32 48 64 96 128 160 192 224 256 288 320 352 384 416 448 480 512 ; do 
+        echo -n "RW/threads=$t:\t" ; 
+        sysbench --test=/usr/share/sysbench/oltp_read_write.lua run --mysql-socket=$MYSQL_SOCK --mysql-user=$MYSQL_USER \
+                --mysql-password=$MYSQL_PASS --tables=64 --table-size=10000000 --threads=$t \
+                --max-time=100 > ./results/sql/oltp_read_write_"$t".txt ; 
+        sleep 10 ; 
+done
